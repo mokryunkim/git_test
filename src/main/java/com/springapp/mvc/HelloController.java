a1c463ed2241b76f94c794a28717c9e7812f8766
황영주 <yeonju1213@gmail.com>
2014-08-27 14:05:10 +0900

    no message

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -11,6 +11,8 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+		model.addAttribute("message1", "Hello world1!");
+
 		return "welcome, peter!!";
 	}
 }
